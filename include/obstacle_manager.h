f651d454a8456ffc7fa2a3b52829b88b28ac3d99
dennis0324 <dennis0324@naver.com>
2022-11-01 02:15:59 +0900

    add: 장애물 속도 동적화, change: 이동 함수 개선

--- include/obstacle_manager.h
+++ include/obstacle_manager.h
@@ -3,23 +3,33 @@
 
 #include "raylib.h"
 #include "obstacle.h"
+#include "player.h"
 
 #define QUEUEELEMENT Obstacle
 #include "queuetype.h"
 
 
 #define OBSTACLE_MOVE_SPEED 10
+#define OBSTACLE_MOVE_SPEED_MUL 0.1
 typedef struct ObstacleManager{
     int timePass;
-    Vector2 size;
+    struct size
+    {
+        int width;
+        int height;
+    }size;
     QueueType *obstacles;
     void (*show)(struct ObstacleManager * ob);
+    Obstacle (*closest)();
+    float moveSpeed;
 }ObstacleManager;
 
 
-ObstacleManager * ObManagerCreate();
+ObstacleManager * ObManagerCreate(int x_size,int y_size);
 
 void Delete_ObManager(ObstacleManager * ob);
+
+Obstacle obstacleClosest(ObstacleManager * ob,Player * p);
 
 #endif
 
