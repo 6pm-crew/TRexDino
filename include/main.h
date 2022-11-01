0c4cb0b843f1c2aa8ee8a1b9f670a44e07ce7476
dennis0324 <dennis0324@naver.com>
2022-11-02 00:50:09 +0900

    add:  추가 및 그에 관련 코딩 추가

---
+++ include/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define TARGET_FPS         90
+
+#define SCREEN_WIDTH       800
+#define SCREEN_HEIGHT      600
+
+#define BACKGROUND_COLOR   ((Color) { 255, 255, 255, 255 })
+#define OBSTABCLE ((Color) {85, 85, 85, 255})
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,19 +1,14 @@
 #include "raylib.h"
 #include "player.h"
 #include "obstacle_manager.h"
+#include "texture_manager.h"
 
-#define TARGET_FPS         60
-
-#define SCREEN_WIDTH       800
-#define SCREEN_HEIGHT      600
-
-#define BACKGROUND_COLOR   ((Color) { 255, 255, 255, 255 })
-#define OBSTABCLE ((Color) {85, 85, 85, 255})
+#include "main.h"
 
 int main(void) {
     // 게임의 목표 FPS를 설정한다.
     SetTargetFPS(TARGET_FPS);
-    SetTraceLogLevel(LOG_DEBUG);
+    // SetTraceLogLevel(LOG_DEBUG);
     // 게임 창을 생성한다.
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "T-Rex Game");
     
@@ -22,6 +17,12 @@ int main(void) {
     const Rectangle ground = {0,0.7f * SCREEN_HEIGHT,SCREEN_WIDTH,0.3f * SCREEN_HEIGHT};
     Player *p = create(SCREEN_WIDTH,SCREEN_HEIGHT);
     ObstacleManager *ob = ObManagerCreate(SCREEN_WIDTH,SCREEN_HEIGHT);
+
+
+    Texture2D texture = LoadTexture("res/images/offline-sprite-2x.png");
+    setPlayerTexture(texture);
+    setObstacleTexture(texture);
+
     // 사용자가 창을 닫거나, `ESC` 키를 누르기 전까지 반복한다.
     while (!WindowShouldClose()) {
         // 게임 화면을 그리기 위해 프레임 버퍼를 초기화한다.
@@ -32,10 +33,14 @@ int main(void) {
 
         // 게임 화면에 어두운 회색 색상의 직사각형을 그린다.
         DrawRectangleRec(bounds, BACKGROUND_COLOR);
-        // DrawRectangleRec(ground,OBSTABCLE);
-        p->show(p);
+        drawBackground(texture,ob->moveSpeed);
         ob->show(ob);
-        obstacleClosest(ob,p);
+        p->show(p);
+
+        Obstacle temp = obstacleClosest(ob,p);
+
+        // if(CheckCollisionRecs(temp.aabb, p->aabb)) break;
+
         // 게임 화면에 현재 FPS를 표시한다.
         DrawFPS(8, 8);
 
@@ -48,6 +53,7 @@ int main(void) {
     // 게임 창에 할당된 메모리를 해제한다.
     DeletePlayer(p);
     Delete_ObManager(ob);
+    UnloadTexture(texture);
     CloseWindow();
 
 
