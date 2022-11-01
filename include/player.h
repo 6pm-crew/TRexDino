db0194ed92095b5f82feef6bda305568b01caaaa
dennis0324 <dennis0324@naver.com>
2022-11-02 00:48:59 +0900

    add: 플레이어 sprite 추가

--- include/player.h
+++ include/player.h
@@ -11,14 +11,15 @@
 #define IDLE_X 0.1f
 #define IDLE_Y 0.7f
 
-#define PLAYER_WIDTH 15.0f
-#define PLAYER_HEIGHT 15.0f
-#define PLAYER_JUMP_SPEED 300
+#define PLAYER_WIDTH 88.0f
+#define PLAYER_HEIGHT 96.0f
+#define PLAYER_JUMP_SPEED 600
 
 typedef struct Player{
     Rectangle aabb;
     Vector2 idle_pos;
     Vector2 velocity;
+    float distance;
     bool isJump;
     void (*move)(struct Player * p);
     void (*show)(struct Player * p);
@@ -27,4 +28,6 @@ typedef struct Player{
 Player * create(float x_size, float y_size);
 
 void DeletePlayer(Player * p);
+
+void setPlayerTexture(Texture tex);
 #endif
--- src/player.c
+++ src/player.c
@@ -1,12 +1,16 @@
 
 #include "player.h"
 #include "raylib.h"
+#include "main.h"
+
 #include <stdlib.h>
 void player_move(Player *player);
 void DrawPlayer(Player *player);
 // static const Vector2 idle_pos;
 static double startJump;
+static Texture texture;
 
+const int INTERVAL = TARGET_FPS / 8;
 Player * create(float x_size, float y_size){
     Player * p = (Player*)malloc(sizeof(Player));
     p->isJump = false;
@@ -15,7 +19,7 @@ Player * create(float x_size, float y_size){
     p->aabb.width = PLAYER_WIDTH;
     p->aabb.height = PLAYER_HEIGHT;
 
-    p->aabb.y = y_size * IDLE_Y;
+    p->aabb.y = y_size * IDLE_Y  - PLAYER_HEIGHT;
     p->idle_pos.y = p->aabb.y;
 
     p->show = DrawPlayer;
@@ -23,6 +27,7 @@ Player * create(float x_size, float y_size){
 }
 
 void ApplyGravity(Player *player) {
+    TraceLog(LOG_DEBUG,"%f",player->aabb.y);
     if (player->aabb.y < player->idle_pos.y) //점프 상태
         player->velocity.y += GRAVITY; //중력 가속도 넣어준다.
 
@@ -31,10 +36,22 @@ void ApplyGravity(Player *player) {
     player->aabb.y += player->velocity.y * deltaTime; //넓이 값 구한다.
 }
 
+
+//좌측 1514 높이 시작 96
+
+// 너비 88
+
 void DrawPlayer(Player *player) {
-    player_move(player);
+    static int frameCounter;
     ApplyGravity(player);
-    DrawRectangleRec(player->aabb, RED);
+    player_move(player);
+    DrawTexturePro(texture,\
+        (Rectangle){.x = 1514 + (frameCounter > INTERVAL) * 88,.y = 0,.width = 88,.height = 96},\
+        player->aabb,\
+        (Vector2){0,0},0,WHITE);
+    frameCounter++;
+    if(frameCounter > INTERVAL *2) frameCounter = 0;
+    // DrawRectangleRec(player->aabb, Fade(RED,0.25f));
 }
 
 
@@ -42,16 +59,20 @@ void DrawPlayer(Player *player) {
 // 가속도 속도 그래프 넓이 = 이동한 거리
 
 void player_move(Player *player) {
+    // printf("%f\n",player->velocity.y);
     if (IsKeyDown(KEY_SPACE) && !player->isJump) {
-        player->isJump = true;
-
         player->velocity.y -= PLAYER_JUMP_SPEED;
+        player->isJump = true;
     }
 
     if (player->aabb.y >= player->idle_pos.y) {
         player->aabb.y = player->idle_pos.y;
         player->isJump = false; 
     }
+}
+
+void setPlayerTexture(Texture text){
+    texture = text;
 }
 
 void DeletePlayer(Player * p){
