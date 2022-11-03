1e12fece9302d1a565742dc1a5d982a839aefdb5
dennis0324 <dennis0324@naver.com>
2022-11-04 00:09:42 +0900

    fix: 플레이어 텔포 현상 수정

--- src/player.c
+++ src/player.c
@@ -42,10 +42,34 @@ const Rectangle textureAABBData[] = {
     }
 };
 
+const Rectangle textruePlayerData[][2] = {
+    {
+        {.x = 1514,.y = 0,.width = 88,.height = 96},
+        {.x = 1514 + 88,.y = 0,.width = 88,.height = 96}
+    },
+    {
+        {.x = 1866,.y = 40,.width = 118,.height = 56},
+        {.x = 1866 + 118,.y = 40,.width = 118,.height = 56},
+    },
+    {
+        {.x = 1689,.y = 0,.width = 88,.height = 96},
+        {.x = 1689,.y = 0,.width = 88,.height = 96}
+    }
+};
+
+enum PlayerDisplayType {
+    PLAYER_ALIVE,
+    PLAYER_ALIVE_LAY,
+    PLAYER_DEAD
+};
+
+extern bool isReload;
+extern bool game_debug;
+
 Player * PlayerCreate(){
     Player * p = (Player*)malloc(sizeof(Player));
     p->isJump = false;
-
+    p->isLaydown = false;
     p->aabb.x = IDLE_X * SCREEN_WIDTH;
     p->aabb.width = PLAYER_WIDTH;
     p->aabb.height = PLAYER_HEIGHT;
@@ -67,59 +91,67 @@ void DeletePlayer(Player * p){
 
 /** 은닉 함수*/
 static void DrawPlayer(Player *player) {
-    static int frameCounter;
-    ApplyGravity(player);
-    player_move(player);
-    // 출력 구현
-    if(!player->isLaydown){
-        player->aabb.width = PLAYER_WIDTH;
-        player->aabb.height = PLAYER_HEIGHT;
-        DrawTexturePro(texture,\
-            (Rectangle){.x = 1514 + (frameCounter > INTERVAL) * 88,.y = 0,.width = 88,.height = 96},\
-            player->aabb,\
-            (Vector2){0,0},0,WHITE);
-    }
-    else{
-        player->aabb.width = PLAYER_WIDTH_LAY;
-        player->aabb.height = PLAYER_HEIGHT_LAY;
-        player->aabb.y = player->aabb.y + PLAYER_HEIGHT - PLAYER_HEIGHT_LAY;
-        DrawTexturePro(texture,\
-            (Rectangle){.x = 1866 + (frameCounter > INTERVAL) * 118,.y = 40,.width = 118,.height = 56},\
-            player->aabb,\
-            (Vector2){0,0},0,WHITE);
-    }
 
+    static int frameCounter;
+    int playerDisplayType = PLAYER_DEAD;
+    if(!isReload){
+        ApplyGravity(player);
+        player_move(player);
+        if(!player->isLaydown){
+            player->aabb.width = PLAYER_WIDTH;
+            player->aabb.height = PLAYER_HEIGHT;
+            playerDisplayType = PLAYER_ALIVE;
+        }
+        else{
+            player->aabb.width = PLAYER_WIDTH_LAY;
+            player->aabb.height = PLAYER_HEIGHT_LAY;
+            playerDisplayType = PLAYER_ALIVE_LAY;
+        }
+    };
+
+    DrawTexturePro(texture,\
+        textruePlayerData[playerDisplayType][frameCounter > INTERVAL],\
+        player->aabb,\
+        (Vector2){0,0},0,WHITE);
+    if(game_debug)
+        DrawRectangleRec(player->aabb, Fade(BLUE,0.25f));
     frameCounter++;
     if(frameCounter > INTERVAL *2) frameCounter = 0;
-    DrawRectangleRec(player->aabb, Fade(RED,0.25f));
 }
 
 static void ApplyGravity(Player *player) {
-    float testing = 1.0f;
-    if(player->isLaydown) testing = 0.2f; // 떨어지는 속도 조절
+    float fallingSpeed = GRAVITY * 3.0f;
 
-    // printf("%d %f\n",player->isLaydown,testing);
-    if (player->aabb.y < player->idle_pos.y) //점프 상태
-        player->velocity.y += GRAVITY; //중력 가속도 넣어준다.
-
+    if (player->aabb.y < player->idle_pos.y){
+        if(!player->isLaydown) player->velocity.y += GRAVITY; //중력 가속도 넣어준다.
+        else player->velocity.y += fallingSpeed;
+    }
+        
+    
     const double deltaTime = GetFrameTime();
 
-    player->aabb.y += player->velocity.y * testing * deltaTime; //넓이 값 구한다.
-    printf("%f\n",player->velocity.y * testing * deltaTime);
+    player->aabb.y += player->velocity.y * deltaTime; //넓이 값 구한다.
+
+    printf("%f\n",player->aabb.y);
 }
 
 
 static void player_move(Player *player) {
-
-    if (player->aabb.y >= player->idle_pos.y) {
+    //
+    if (player->aabb.y >= player->idle_pos.y + player->isLaydown * 40) {
         player->aabb.y = player->idle_pos.y;
-        if(player->isJump) player->velocity.y = 0;
         player->isJump = false; 
+        player->velocity.y = 0;
+        player->isLaydown = false;
     }
 
     if ((IsKeyDown(KEY_SPACE) || IsKeyDown(KEY_UP)) && !player->isJump) {
         player->velocity.y -= PLAYER_JUMP_SPEED;
         player->isJump = true;
     }
-    player->isLaydown = IsKeyDown(KEY_DOWN);
+
+    if (IsKeyDown(KEY_DOWN) && !player->isLaydown){
+        player->isLaydown = true;
+        player->aabb.y += (PLAYER_HEIGHT - PLAYER_HEIGHT_LAY);
+    }
 }
