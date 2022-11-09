855bda3fdcc06fc5385b56907236e5368d7628fe
dennis0324 <dennis0324@naver.com>
2022-11-09 23:53:07 +0900

    change: 명령어 정리

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import cv2
 import time
 import win32gui
 import numpy as np
+from DQNTrainer import DQNTrainer  
+
 
 print("running")
 ESC_KEY=27
@@ -10,16 +12,15 @@ FRAME_RATE = 90
 SLEEP_TIME = 1/FRAME_RATE
 
 capture = window.WindowCapture("T-Rex Game",FRAME_RATE)
+trainer = DQNTrainer()
+
 
 while True:
     start=time.time()
     frame = capture.screenshot()
-    x_train = np.array(frame)
-    print(x_train.shape)
     cv2.imshow("frame1",frame)
+    print(trainer.isGameOver(frame))
     delta= time.time()-start
-    # if delta <SLEEP_TIME:
-    #     time.sleep(SLEEP_TIME-delta)
     key= cv2.waitKey(1) & 0xFF
     if cv2.getWindowProperty("frame1", cv2.WND_PROP_VISIBLE) <1:
         break
